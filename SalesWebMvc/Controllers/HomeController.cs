d0eafb4406462956b8f01295e88c49ecdbd006a6
Israel Junior <israelljunnior15987426@hotmail.com>
2020-01-24 08:22:14 -0300

    First Controller and Razor tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course";
+            ViewData["Email"] = "israel@gmail";
+
 
             return View();
         }
