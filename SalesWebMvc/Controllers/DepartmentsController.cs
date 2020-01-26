4b74911848d153fbf08490e856def057b56a3245
Israel Junior <israelljunnior15987426@hotmail.com>
2020-01-26 15:45:43 -0300

    First View

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name= "Eletronics" });
+            list.Add(new Department { Id = 2, Name= "Fashion" });
+
+            return View(list);
+        }
+    }
+}
