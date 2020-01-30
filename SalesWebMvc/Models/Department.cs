9381c2cba337f383479647ae3176d51be3d9cd90
Israel Junior <israelljunnior15987426@hotmail.com>
2020-01-30 07:51:52 -0300

    Other Enteties and Second Migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -9,5 +9,27 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSellers(DateTime initial, DateTime final) {
+
+            return Sellers.Sum(sr => sr.TotalSales(initial, final));
+
+        }
     }
 }
